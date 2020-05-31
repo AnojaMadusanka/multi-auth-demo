2d65fbe65bbbeedf66e9f6b3343054c4fb49e3c8
anojaM <anojamadusanka7@gmail.com>
2020-05-31 14:05:39 +0530

    Add demo projects

---
+++ multi-auth/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Admin;
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\User;
+use App\Writer;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+        $this->middleware('guest:admin');
+        $this->middleware('guest:writer');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param array $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param array $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+
+    //Show Admin Registration Form
+    public function showAdminRegistrationForm()
+    {
+        return view('auth.register', ['url' => 'admin']);
+    }
+
+    //Show Writer Login Form
+    public function showWriterRegistrationForm()
+    {
+        return view('auth.register', ['url' => 'writer']);
+    }
+
+    //method for creating anAdmin
+    protected function createAdmin(Request $request)
+    {
+        $this->validator($request->all())->validate();
+        Admin::create([                                                     //$admin = Admin:: like that we can assign to local variable
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+        return redirect()->intended('login/admin');
+    }
+
+    //method for creating a writer
+    protected function createWriter(Request $request)
+    {
+        $this->validator($request->all())->validate();
+        Writer::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+        return redirect()->intended('login/writer');
+    }
+}
---
+++ multi-auth/resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.auth')
+@section('content')
+    <div class="col-sm">
+        <h1 style="color: #9561e2">404 PAGE NOT FOUND</h1>
+    </div>
+@endsection
